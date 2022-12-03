c5be98bfb40cc1d8548d3ddccb6e62047a0e0dd2
Kristian2705 <k_petrov27@abv.bg>
2022-12-03 15:46:50 +0200

    Added Project
    as

---
+++ ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ParkingSystem.Data;
+using ParkingSystem.Data.Models;
+using System.Linq;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController
+    {
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            DataAccess.Cars.Add(car);
+            var res = new RedirectResult("/");
+            return res;
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber) 
+        {
+            var carToRemove = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+            DataAccess.Cars.Remove(carToRemove);
+            var res = new RedirectResult("/");
+            return res;
+        }
+    }
+}
